682e1830e1f1ccd6e85ff0f7e8de0ab853907f38
Angus Webb <AW079@atcltd.atc.org.nz>
2023-02-09 16:34:07 +1300

    Rebuild

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Quiz from './Components/Quiz';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Quiz />
+  </React.StrictMode>,
+ document.getElementById('root')
+ );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
